ca8fd66662ad2e30b2b8f685caf6e1a8903ff33c
Ethan <ethtaylan@gmail.com>
2023-07-27 12:16:13 +0200

    feat: cors policy

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,10 @@
+const crypto = require("crypto");
 
 module.exports = ({ env }) => ({
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+    salt: env("API_TOKEN_SALT", crypto.randomBytes(16).toString("base64")),
   },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '77b2c87dbab4e1697bec244226fbd1b3'),
+    secret: env("ADMIN_JWT_SECRET", crypto.randomBytes(16).toString("base64")),
   },
 });
